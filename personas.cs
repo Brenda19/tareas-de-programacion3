7e3c2b28dc4e6b4ec040c666e927e7f3b8c69d8c
brenda19 <brenda.herrera@alumno.udg.mx>
2013-09-18 19:23:59 -0500

    tarea1

---
+++ personas.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: hp
+ * Date: 18/09/2013
+ * Time: 05:38 p.m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace practica2
+{
+	/// <summary>
+	/// Description of Persona.
+	/// </summary>
+	public class Persona
+	{
+		
+		    public string nombre;
+			public string edad;
+			public string correo;
+			
+		public Persona()
+		{
+			
+			
+		}
+	}
+}
---
+++ program.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: hp
+ * Date: 18/09/2013
+ * Time: 05:24 p.m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace practica2
+{
+	class tarea
+	{
+		public static void Main(string[] args)
+		{
+			
+			
+			 Persona p =new Persona();
+			 
+			 Console.WriteLine("Introduzca su nombre");
+			 p.nombre=Console.ReadLine();
+             Console.WriteLine("Nombre: "+p.nombre);
+			 
+			 Console.WriteLine("Introduzca su edad");
+			 p.edad=Console.ReadLine();
+			 Console.WriteLine("Edad: "+p.edad);
+			 
+			 Console.WriteLine("Introduzca su correo");
+			 p.correo=Console.ReadLine();
+			 Console.WriteLine("Correo: "+p.correo);
+			 Console.Write("Press any key to continue . . . ");
+			 Console.ReadKey(true);
+		}
+	}
+}
